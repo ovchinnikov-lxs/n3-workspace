e879776e5ea53f34f15fa492d85bd53e92e441f6
Alexander Ovchinnikov <a.ovchinnikov@wincorp.team>
2024-08-23 08:58:18 +0400

    feat(portfolio): added gtm tag

--- apps/portfolio/app.vue
+++ apps/portfolio/app.vue
@@ -50,6 +50,14 @@ useHead({
     ],
     script: () => [
         {
+            innerHTML:
+                "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n" +
+                "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n" +
+                "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n" +
+                "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n" +
+                "})(window,document,'script','dataLayer','GTM-KLDBHXW4');",
+        },
+        {
             type: 'application/ld+json',
             innerHTML: {
                 '@context': 'https://schema.org',
