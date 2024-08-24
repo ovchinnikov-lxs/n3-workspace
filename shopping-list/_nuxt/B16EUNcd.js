7133b861845d17be720ac5338f560068aa50c4cd
ovchinnikov-lxs <ovchinnikov-lxs@users.noreply.github.com>
2024-08-24 03:43:20 +0000

    deploy: 451244a5b7405597b576f40568096642dcb03a5f

---
+++ shopping-list/_nuxt/B16EUNcd.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./BexkF9yi.js";import"./CVybZrq0.js";export{m as default};
---
+++ shopping-list/_nuxt/BuZ_Kxxj.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DFEpQlXc.js";import"./DPrRWt3M.js";import"./CVybZrq0.js";export{o as default};
