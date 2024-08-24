a06b9d77a1f08d338311d896285c6a54a3f1ea7d
Alexander Ovchinnikov <a.ovchinnikov@wincorp.team>
2024-08-24 08:35:25 +0400

    refactor(portfolio): move to onNuxtReady cb gtm scripts init

--- apps/portfolio/app.vue
+++ apps/portfolio/app.vue
@@ -54,26 +54,6 @@ useHead({
     ],
     script: () => [
         {
-            innerHTML:
-                "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n" +
-                "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n" +
-                "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n" +
-                "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n" +
-                "})(window,document,'script','dataLayer','GTM-KLDBHXW4');",
-        },
-        {
-            async: true,
-            src: 'https://www.googletagmanager.com/gtag/js?id=G-8BZP9D108T',
-        },
-        {
-            innerHTML:
-                '  window.dataLayer = window.dataLayer || [];\n' +
-                '  function gtag(){dataLayer.push(arguments);}\n' +
-                "  gtag('js', new Date());\n" +
-                '\n' +
-                "  gtag('config', 'G-8BZP9D108T');",
-        },
-        {
             type: 'application/ld+json',
             innerHTML: {
                 '@context': 'https://schema.org',
@@ -96,6 +76,33 @@ useHead({
             },
         },
     ],
+});
+
+onNuxtReady(() => {
+    useHead({
+        script: [
+            {
+                innerHTML:
+                    "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n" +
+                    "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n" +
+                    "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n" +
+                    "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n" +
+                    "})(window,document,'script','dataLayer','GTM-KLDBHXW4');",
+            },
+            {
+                async: true,
+                src: 'https://www.googletagmanager.com/gtag/js?id=G-8BZP9D108T',
+            },
+            {
+                innerHTML:
+                    '  window.dataLayer = window.dataLayer || [];\n' +
+                    '  function gtag(){dataLayer.push(arguments);}\n' +
+                    "  gtag('js', new Date());\n" +
+                    '\n' +
+                    "  gtag('config', 'G-8BZP9D108T');",
+            },
+        ],
+    });
 });
 
 useSeoMeta({
