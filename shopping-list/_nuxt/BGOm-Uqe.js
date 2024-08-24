27a04242dae017c815127a72650a295dc0dd0458
ovchinnikov-lxs <ovchinnikov-lxs@users.noreply.github.com>
2024-08-24 04:14:08 +0000

    deploy: 36a2b22e29b2da37e7063599cef0803fd20639d4

---
+++ shopping-list/_nuxt/BGOm-Uqe.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./C97c2j32.js";import"./DBuHJA5P.js";import"./Dj6VA0ba.js";export{o as default};
---
+++ shopping-list/_nuxt/W9oF5F7n.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./Cbjy8N6p.js";import"./Dj6VA0ba.js";export{m as default};
