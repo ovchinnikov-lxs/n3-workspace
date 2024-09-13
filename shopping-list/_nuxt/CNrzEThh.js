bb32668c7ee829eba7e15d0f7d52b62a1df64c84
ovchinnikov-lxs <ovchinnikov-lxs@users.noreply.github.com>
2024-09-13 05:29:51 +0000

    deploy: a7d8e979e5e34475113ea6e6d58051e0867cdad6

---
+++ shopping-list/_nuxt/CNrzEThh.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,z as c,aa as n}from"./CGYY1x1z.js";const _={};function a(r,s){const e=n;return t(),c(e)}const f=o(_,[["render",a]]);export{f as default};
---
+++ shopping-list/_nuxt/D3dTaAyO.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./DAGu6hy0.js";import"./CGYY1x1z.js";export{m as default};
