fdaf3d5d4a6d0211d9ca9a45562023b01a25693c
ovchinnikov-lxs <ovchinnikov-lxs@users.noreply.github.com>
2024-11-09 20:15:50 +0000

    deploy: bd89d64cbff322ca89444f5e8dfe8b144feb002b

---
+++ shopping-list/_nuxt/BeawMO3I.js
@@ -0,0 +1,1 @@
+import{L as s}from"./ZljE1_sI.js";const u=()=>{var e;return(e=s().$supabase)==null?void 0:e.client};export{u};
---
+++ shopping-list/_nuxt/q2nnlAfh.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./CB2FFdSs.js";import"./ZljE1_sI.js";export{m as default};
